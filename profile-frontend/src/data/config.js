6ca1a12d33eb72347a51dcd52829db00d7c778ca
skmitra <mitrakumarsujan@gmail.com>
2021-01-19 20:53:40 +0530

    added personal info

--- profile-frontend/src/data/config.js
+++ profile-frontend/src/data/config.js
@@ -1,10 +1,10 @@
 
 module.exports = {
   // TODO: change your profile information here
-  name: "Ashok Kumar",
+  name: "Sujan Kumar Mitra",
   greeting: "Hey 👋",
-  greetingDescription: "I'm Ashok Kumar and I'm a Software Engineer!",
-  githubUrl: "https://github.com/ashokkumar95",
-  linkedinUrl: "https://linkedin.com/ashokkumar1",
-  cvLink: "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
+  greetingDescription: "I'm Sujan Kumar Mitra and I'm a Software Engineer!",
+  githubUrl: "https://github.com/SujanKumarMitra",
+  linkedinUrl: "https://www.linkedin.com/in/sujan-kumar-mitra-1b6913174/",
+  cvLink: "https://drive.google.com/file/d/1H5bV3-4eNH3YeGWeWCBjJBFrnsttd0SD/view?usp=sharing",
 };
